809e556e049de31e3805174b05639c0547a6f17a
Naser ARAB <naser.arab@dreamix.eu>
2022-05-30 20:00:39 +0300

    Add project files.

---
+++ ChatService/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatService.Hubs
+{
+    public class ChatHub: Hub
+    {
+        public async Task SendMessage(string userName,string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", userName, message);
+        }
+    }
+}
